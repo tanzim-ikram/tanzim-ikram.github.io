c1c3a680fbc4c6248653ba5589160811797d1daa
Tanzim Ikram Sheikh <tanzim.ikram@gmail.com>
2026-07-03 15:20:52 +0600

    feat: update portfolio with publication list, education and experience sections, and site metadata

--- scripts.js
+++ scripts.js
@@ -106,7 +106,7 @@ function createPublicationElement(publication) {
   // Format authors with highlighting
   let authorsHTML = '';
   publication.authors.forEach((author, index) => {
-    if (author.includes('Author 3')) { // TODO: Highlight specific author
+    if (author.includes('T. I. Sheikh')) { // Highlight Tanzim Ikram Sheikh
       authorsHTML += `<span class="highlight-name">${author}</span>`;
     } else {
       authorsHTML += author;
